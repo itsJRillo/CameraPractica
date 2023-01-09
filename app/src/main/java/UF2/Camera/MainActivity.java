b0d29cc5b7bd4f710c83d477d205e21bdb2e818e
Ismael M <jmorillolabour@gmail.com>
2023-01-09 16:46:41 +0100

    Open Thumb

--- app/src/main/java/UF2/Camera/MainActivity.java
+++ app/src/main/java/UF2/Camera/MainActivity.java
@@ -1,18 +1,32 @@
 package UF2.Camera;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
     private static final String[] CAMERA_PERMISSION = new String[]{Manifest.permission.CAMERA};
     private static final int CAMERA_REQUEST_CODE = 10;
+
+    public static int RC_PHOTO_PICKER = 0;
+    public ActivityResultLauncher<Intent> someActivityResultLauncher = null;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +41,43 @@ public class MainActivity extends AppCompatActivity {
                 requestPermission();
             }
         });
+
+        someActivityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == Activity.RESULT_OK) {
+                            // There are no request codes
+                            Intent data = result.getData();
+                            Uri uri = data.getData();
+                            ImageView imageView = findViewById(R.id.img);
+                            imageView.setImageURI(uri);
+                        }
+                    }
+                }
+        );
+
+        Button gallery = findViewById(R.id.gallery);
+        gallery.setOnClickListener(v -> {
+            Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+            intent.setType("image/jpg");
+            intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
+            someActivityResultLauncher.launch(intent);
+        });
+
+
     }
+
+    /*
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imageBitmap);
+        }
+    }
+    */
 
     private boolean hasCameraPermission() {
         return ContextCompat.checkSelfPermission(
